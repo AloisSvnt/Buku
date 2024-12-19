6ac0a20868d1babd316040fbd0f30cc010eff2b3
AloisSvnt <alois.sauvonnet@digitalconcept.fr>
2024-12-19 14:49:33 +0100

    feat: create migration for reviews table

---
+++ database/migrations/1734616090617_create_reviews_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'reviews'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('book_id').unsigned().references('id').inTable('books').onDelete('CASCADE')
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table.integer('rating').notNullable()
+      table.text('review').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
