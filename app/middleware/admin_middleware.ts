63aea04e7ca9ccd7214c3f1eaad650c107baf67d
AloisSvnt <alois.sauvonnet@digitalconcept.fr>
2024-12-19 15:00:55 +0100

    feat: create admin middleware

---
+++ app/middleware/admin_middleware.ts
@@ -0,0 +1,27 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import type { NextFn } from '@adonisjs/core/types/http'
+import type { Authenticators } from '@adonisjs/auth/types'
+
+export default class AdminMiddleware {
+  /**
+   * The URL to redirect to, when authentication fails
+   */
+  redirectTo = '/login'
+
+  async handle(
+    ctx: HttpContext,
+    next: NextFn,
+    options: {
+      guards?: (keyof Authenticators)[]
+    } = {}
+  ) {
+    await ctx.auth.authenticateUsing(options.guards, { loginRoute: this.redirectTo })
+
+    const user = ctx.auth.user
+    if (!user || !user.isAdmin) {
+      return ctx.response.redirect(this.redirectTo)
+    }
+
+    return next()
+  }
+}
