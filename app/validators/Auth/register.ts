07300365fbd43f1b727983994da0854235e2e354
AloisSvnt <alois.sauvonnet@digitalconcept.fr>
2024-12-19 15:20:05 +0100

    feat: create registerValidator

---
+++ app/validators/Auth/register.ts
@@ -0,0 +1,20 @@
+import vine from '@vinejs/vine'
+
+export const registerValidator = vine.compile(
+  vine.object({
+    fullName: vine.string().minLength(3).maxLength(255),
+    email: vine
+      .string()
+      .email()
+      .unique(async (db, value) => {
+        const user = await db.from('users').where('email', value).first()
+        return !user
+      }),
+    password: vine
+      .string()
+      .minLength(8)
+      .maxLength(90)
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/),
+    isAdmin: vine.boolean()
+  })
+)
