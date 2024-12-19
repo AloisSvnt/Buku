0115a96c7ea9c375bccf3c2124806ea7a66132a5
AloisSvnt <alois.sauvonnet@digitalconcept.fr>
2024-12-19 14:45:15 +0100

    feat: create migration for books table

---
+++ database/migrations/1734615758072_create_books_table.ts
@@ -0,0 +1,27 @@
+import Category from '#models/category'
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'books'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title').notNullable()
+      table.text('description').notNullable()
+      table.integer('category_id').unsigned().references('id').inTable('categories').notNullable()
+      table.integer('author_id').unsigned().references('id').inTable('authors').notNullable()
+      table.boolean('is_new').defaultTo(false)
+      table.boolean('is_popular').defaultTo(false)
+      table.boolean('is_recommended').defaultTo(false)
+      table.boolean('is_on_sale').defaultTo(false)
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
