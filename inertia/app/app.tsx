967c6babf76967b6270e85e74cca88c5be2afc6b
AloisSvnt <alois.sauvonnet@digitalconcept.fr>
2024-12-20 10:58:15 +0100

    feat: create persistant layout

--- inertia/app/app.tsx
+++ inertia/app/app.tsx
@@ -8,16 +8,23 @@ import { resolvePageComponent } from '@adonisjs/inertia/helpers'
 
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
 
+import AdminLayout from '~/layouts/AdminLayout';
+import FrontLayout from '~/layouts/FrontLayout';
+
 createInertiaApp({
   progress: { color: '#5468FF' },
 
   title: (title) => `${title} - ${appName}`,
 
-  resolve: (name) => {
-    return resolvePageComponent(
-      `../pages/${name}.tsx`,
-      import.meta.glob('../pages/**/*.tsx'),
-    )
+  resolve: async (name: string) => {
+    const pages = import.meta.glob('../pages/**/*.tsx', { eager: true }) as Record<string, { default: any }>;
+    const page = await resolvePageComponent(`../pages/${name}.tsx`, pages) as { default: any };
+    
+    name.startsWith('admin') ? 
+      page.default.layout = AdminLayout : 
+      page.default.layout = FrontLayout;
+
+    return page;
   },
 
   setup({ el, App, props }) {
