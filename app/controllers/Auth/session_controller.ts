d3a9606729940fde9a18479889cdbb02a4a0b8a4
AloisSvnt <alois.sauvonnet@live.fr>
2024-12-21 09:26:19 +0100

    feat(Auth/SessionController): remove console.log

--- app/controllers/Auth/session_controller.ts
+++ app/controllers/Auth/session_controller.ts
@@ -4,18 +4,16 @@ import { registerValidator } from '#validators/Auth/register'
 import { loginValidator } from '#validators/Auth/login'
 
 export default class SessionController {
-
   async register({ request, auth, response, session }: HttpContext) {
     const { fullName, email, password, isAdmin } = await request.validateUsing(registerValidator)
 
     try {
       const user = await User.create({ email, password, fullName, isAdmin })
       await auth.use('web').login(user)
-      session.flash({success: 'You are registered and logged in.'})
+      session.flash({ success: 'You are registered and logged in.' })
       return response.redirect('/')
-
     } catch (error) {
-      session.flash({error: 'An error occurred while registering. Please try again.'})
+      session.flash({ error: 'An error occurred while registering. Please try again.' })
       return response.redirect('/register')
     }
   }
@@ -26,23 +24,21 @@ export default class SessionController {
     try {
       const user = await User.verifyCredentials(email, password)
       await auth.use('web').login(user)
-      if(user.isAdmin) {
-        console.log('Admin')
-        session.flash({success: `You are logged in as ${user.fullName}.`})
+      if (user.isAdmin) {
+        session.flash({ success: `You are logged in as ${user.fullName}.` })
         return response.redirect('/admin/dashboard')
       }
-      session.flash({success: 'You are logged in.'})
+      session.flash({ success: 'You are logged in.' })
       return response.redirect('/')
-
     } catch (error) {
-      session.flash({error: 'Incorrect credentials.'})
+      session.flash({ error: 'Incorrect credentials.' })
       return response.redirect('/login')
     }
   }
 
   async logout({ auth, response, session }: HttpContext) {
     await auth.use('web').logout()
-    session.flash({success: 'You are logged out.'})
+    session.flash({ success: 'You are logged out.' })
     return response.redirect('/')
   }
 
