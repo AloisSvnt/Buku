72d65d8208e9cd8ad0a09a92e9ed432eed04746a
AloisSvnt <alois.sauvonnet@digitalconcept.fr>
2024-12-30 11:19:43 +0100

    feat: add links in drawer

--- inertia/layouts/AdminLayout.tsx
+++ inertia/layouts/AdminLayout.tsx
@@ -10,7 +10,7 @@ function AdminLayout(children: React.ReactNode) {
       <main className='flex-1'>
         <div className="drawer lg:drawer-open lg:h-[calc(100vh-4rem)]">
           <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content flex flex-col items-center justify-center">
+          <div className="drawer-content flex flex-col items-center justify-center p-4">
             {children}
             <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
               Open drawer
@@ -19,14 +19,16 @@ function AdminLayout(children: React.ReactNode) {
           <div className="drawer-side h-full">
             <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
             <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-              <Link href="" className="join-item btn btn-neutral">Sidebar Item 1</Link>
-              <Link href="" className="join-item btn btn-neutral">Sidebar Item 2</Link>
-              <Link href="" className="join-item btn btn-neutral">Sidebar Item 1</Link>
-              <Link href="" className="join-item btn btn-neutral">Sidebar Item 2</Link>
-              <Link href="" className="join-item btn btn-neutral">Sidebar Item 1</Link>
-              <Link href="" className="join-item btn btn-neutral">Sidebar Item 2</Link>
-              <Link href="" className="join-item btn btn-neutral">Sidebar Item 1</Link>
-              <Link href="" className="join-item btn btn-neutral">Sidebar Item 2</Link>
+              <a href="/" target="_blank" className="btn btn-ghost hover:bg-transparent hover:underline">Front page</a>
+              <Link href="/admin/dashboard" className="btn btn-ghost hover:bg-transparent hover:underline">Dashboard</Link>
+              <Link href="/admin/books" className="btn btn-ghost hover:bg-transparent hover:underline">Books</Link>
+              <Link href="" className="btn btn-ghost hover:bg-transparent hover:underline">Sidebar Item 2</Link>
+              <Link href="" className="btn btn-ghost hover:bg-transparent hover:underline">Sidebar Item 1</Link>
+              <Link href="" className="btn btn-ghost hover:bg-transparent hover:underline">Sidebar Item 2</Link>
+              <Link href="" className="btn btn-ghost hover:bg-transparent hover:underline">Sidebar Item 1</Link>
+              <Link href="" className="btn btn-ghost hover:bg-transparent hover:underline">Sidebar Item 2</Link>
+              <Link href="" className="btn btn-ghost hover:bg-transparent hover:underline">Sidebar Item 1</Link>
+              <Link href="" className="btn btn-ghost hover:bg-transparent hover:underline">Sidebar Item 2</Link>
             </ul>
           </div>
         </div>
