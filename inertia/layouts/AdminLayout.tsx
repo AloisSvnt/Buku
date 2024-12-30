07cda51691f885629f9c8bfff477c583e2028244
AloisSvnt <alois.sauvonnet@digitalconcept.fr>
2024-12-30 12:03:29 +0100

    style: remove padding on children container

--- inertia/layouts/AdminLayout.tsx
+++ inertia/layouts/AdminLayout.tsx
@@ -12,7 +12,7 @@ function AdminLayout(children: React.ReactNode) {
       <main className='flex-1'>
         <div className="drawer lg:drawer-open lg:h-[calc(100vh-4rem)]">
           <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content flex flex-col items-center justify-center p-4">
+          <div className="drawer-content flex flex-col items-center justify-center">
             {children}
             <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
               Open drawer
