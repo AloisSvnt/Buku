4e2b62ea714d0846bbee190cb7603d12e54eab2f
AloisSvnt <alois.sauvonnet@digitalconcept.fr>
2024-12-30 11:35:30 +0100

    style(layouts/adminLayout): use Iconify for in drawer

--- inertia/layouts/AdminLayout.tsx
+++ inertia/layouts/AdminLayout.tsx
@@ -1,3 +1,5 @@
+import { Icon } from "@iconify/react";
+
 import { Link } from "@inertiajs/react";
 import HeaderAdmin from "~/components/headers/HeaderAdmin";
 import FooterAdmin from "~/components/footers/FooterAdmin";
@@ -19,16 +21,20 @@ function AdminLayout(children: React.ReactNode) {
           <div className="drawer-side h-full">
             <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
             <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-              <a href="/" target="_blank" className="btn btn-ghost hover:bg-transparent hover:underline">Front page</a>
-              <Link href="/admin/dashboard" className="btn btn-ghost hover:bg-transparent hover:underline">Dashboard</Link>
-              <Link href="/admin/books" className="btn btn-ghost hover:bg-transparent hover:underline">Books</Link>
-              <Link href="" className="btn btn-ghost hover:bg-transparent hover:underline">Sidebar Item 2</Link>
-              <Link href="" className="btn btn-ghost hover:bg-transparent hover:underline">Sidebar Item 1</Link>
-              <Link href="" className="btn btn-ghost hover:bg-transparent hover:underline">Sidebar Item 2</Link>
-              <Link href="" className="btn btn-ghost hover:bg-transparent hover:underline">Sidebar Item 1</Link>
-              <Link href="" className="btn btn-ghost hover:bg-transparent hover:underline">Sidebar Item 2</Link>
-              <Link href="" className="btn btn-ghost hover:bg-transparent hover:underline">Sidebar Item 1</Link>
-              <Link href="" className="btn btn-ghost hover:bg-transparent hover:underline">Sidebar Item 2</Link>
+              <a href="/" target="_blank" className="btn btn-ghost hover:bg-transparent hover:underline justify-start">
+                <Icon icon="mdi:home" width="24" height="24" />
+                Front page
+              </a>
+              {[
+                { href: "/admin/dashboard", icon: "mdi:view-dashboard-outline", label: "Dashboard" },
+                { href: "/admin/books", icon: "mdi:bookshelf", label: "Books" },
+                { href: "/admin/users", icon: "mdi:users", label: "Users" },
+              ].map((item) => (
+                <Link key={item.href} href={item.href} className="btn btn-ghost hover:bg-transparent hover:underline justify-start">
+                  <Icon icon={item.icon} width="24" height="24" />
+                  {item.label}
+                </Link>
+              ))}
             </ul>
           </div>
         </div>
